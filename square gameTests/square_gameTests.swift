321a8a8181bf88ab9f032efcd7696b979f3291af
Gayan28 <gayankavinda28@gmail.com>
2025-01-05 13:57:20 +0530

    Initial Commit

---
+++ square gameTests/square_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  square_gameTests.swift
+//  square gameTests
+//
+//  Created by Gayan 033 on 2025-01-05.
+//
+
+import Testing
+@testable import square_game
+
+struct square_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
